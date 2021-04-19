aa4a00d9a0285bdb819be995538785125b8f780a
yijinfang11 <81932557+yijinfang11@users.noreply.github.com>
2021-04-18 20:26:06 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import Home from './pages/home';
 import ReviewPage from './pages/review';
 import ReviewForm from './pages/form';
 import { data } from './pages/home/movie-data.js';
-
+import './pages/review/format.css';
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* put Header here to apply to all pages */}
+        {<div><h1>Movie Photosensitivity Review Website</h1>
+          </div>}
         <Switch>
           <Route exact path={'/'} >
             <Home data={data} />
